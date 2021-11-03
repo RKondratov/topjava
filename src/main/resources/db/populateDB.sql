33155076c50b01d6130d8edd7764025c0c263b4b
Kondratov Rustam <procon89@mail.ru>
2021-11-03 21:51:36 +0300

    HW03 base

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM meals;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +13,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES (to_timestamp('30.01.2020 10:00:00', 'DD.MM.YYYY HH24:MI:SS'), 'Завтрак', 500, 100000),
+       (to_timestamp('30.01.2020 13:00:00', 'DD.MM.YYYY HH24:MI:SS'), 'Обед', 1000, 100000),
+       (to_timestamp('30.01.2020 20:00:00', 'DD.MM.YYYY HH24:MI:SS'), 'Ужин', 500, 100000),
+       (to_timestamp('31.01.2020 00:00:00', 'DD.MM.YYYY HH24:MI:SS'), 'Еда на граничное значение', 100, 100000),
+       (to_timestamp('31.01.2020 10:00:00', 'DD.MM.YYYY HH24:MI:SS'), 'Завтрак', 1000, 100000),
+       (to_timestamp('31.01.2020 13:00:00', 'DD.MM.YYYY HH24:MI:SS'), 'Обед', 500, 100000),
+       (to_timestamp('31.01.2020 20:00:00', 'DD.MM.YYYY HH24:MI:SS'), 'Ужин', 410, 100000);
